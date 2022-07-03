13b09830f502bf9f32726ec5a1c623bf81c4f86c
socphoenix <tuc03516@gmail.com>
2022-07-03 06:32:58 -0600

    Update sql.h

--- sql.h
+++ sql.h
@@ -1,4 +1,5 @@
-/*This program is Licensed under the GNU GPL V3 */
+/*This program is Licensed under the GNU GPL V3. This program uses the Qt Sql library, which is
+licensed under the GNU LGPL V3 https://www.gnu.org/licenses/lgpl-3.0.html*/
 #ifndef SQL_H
 #define SQL_H
 
