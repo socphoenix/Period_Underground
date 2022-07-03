5d8c4a934bd90baea6d3bd729b5f39c737a96cd7
socphoenix <tuc03516@gmail.com>
2022-07-03 09:20:55 -0600

    add notes to about on usage

--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,7 @@
-/*This program is Licensed under the GNU GPL V3 */
+/*This program is Licensed under the GNU GPL V3. This program uses Qt opensource
+ * which uses the GPL V3, except for the Qt Sql library. The Qt Sql library is
+ *licensed under the GNU LGPL V3 https://www.gnu.org/licenses/lgpl-3.0.html
+ * This program also uses SQlite which is in the public domain, see sqlite.org for more information.*/
 
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
--- sql.h
+++ sql.h
@@ -1,5 +1,7 @@
-/*This program is Licensed under the GNU GPL V3. This program uses the Qt Sql library, which is
-licensed under the GNU LGPL V3 https://www.gnu.org/licenses/lgpl-3.0.html*/
+/*This program is Licensed under the GNU GPL V3. This program uses Qt opensource
+ * which uses the GPL V3, except for the Qt Sql library. The Qt Sql library is
+ *licensed under the GNU LGPL V3 https://www.gnu.org/licenses/lgpl-3.0.html
+ * This program also uses SQlite which is in the public domain, see sqlite.org for more information.*/
 #ifndef SQL_H
 #define SQL_H
 
